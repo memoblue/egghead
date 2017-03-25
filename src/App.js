7ec5fe38cf5a9305f4c8bc570c2d574c75d69fd9
memo <me@memo.blue>
2017-03-25 16:30:31 -0400

    add state

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
 
 class App extends React.Component {
+  constructor() {
+    super(); // give `this` the component's context instead of parent class
+    this.state = {
+      txt: 'This is some text from state.'
+    }
+  }
   render() {
-    const txt = this.props.txt;
+    const propTxt = this.props.txt;
+    const stateTxt = this.state.txt;
     return (
       <div>
         <h1>A class component</h1>
         <p>A componenet's render methode can only return one <code>React.createElement()</code> so we nest the JSX in a top <code>div</code>.</p>
-        <p>{txt}</p>
+        <p>{propTxt}</p>
+        <p>{stateTxt}</p>
       </div>
     );
   }
