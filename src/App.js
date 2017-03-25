62bca79970d34097791314b51d8de0a780647a2c
memo <me@memo.blue>
2017-03-25 16:37:01 -0400

    add custom method to change the state

--- src/App.js
+++ src/App.js
@@ -7,6 +7,13 @@ class App extends React.Component {
       txt: 'This is some text from state.'
     }
   }
+
+  myUpdate(e) {
+    this.setState({
+      txt: e.target.value
+    })
+  }
+
   render() {
     const propTxt = this.props.txt;
     const stateTxt = this.state.txt;
@@ -16,6 +23,7 @@ class App extends React.Component {
         <p>A componenet's render methode can only return one <code>React.createElement()</code> so we nest the JSX in a top <code>div</code>.</p>
         <p>{propTxt}</p>
         <p>{stateTxt}</p>
+        <input onChange={this.myUpdate.bind(this)} />
       </div>
     );
   }
